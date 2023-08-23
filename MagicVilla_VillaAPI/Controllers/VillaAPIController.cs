a9d7b2e012ebe7c0bc5d402a9539dd444dc0702b
iamakhildabral <akhildabral02@gmail.com>
2023-08-23 19:17:37 +0530

    added response code for the end points

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,6 +1,45 @@
-﻿namespace MagicVilla_VillaAPI.Controllers
+﻿
+using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace MagicVilla_VillaAPI.Controllers
 {
-    public class VillaAPIController
+    [Route("api/VillaAPI")] //we can also use api/[controller]
+    [ApiController]
+    public class VillaAPIController: ControllerBase
     {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+
+            return Ok(VillaStore.villaList) ;
+
+
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVillaAsync(int id)
+        {         
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            if(id <= 0)
+            {
+                return BadRequest("Incorrect ID");
+            }
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+ 
     }
 }
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_VillaAPI.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+            {
+                new VillaDTO{Id=1,Name= "Pool-View"},
+                new VillaDTO{Id=2,Name= "Beach-View"},
+                new VillaDTO { Id = 3, Name = "High-View" }
+
+            };
+    }   
+}
